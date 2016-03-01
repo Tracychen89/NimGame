368ba998d4f99b2263b0e5575ac14e2e31a93403
Tracychen89 <xinchen1215@gmail.com>
2016-03-01 15:54:19 -0800

    My solution

---
+++ NimGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public bool CanWinNim(int n)
+        {
+            // if the request is 1-m stones, we can solve this problem by change 4 to m+1
+
+            if (n % 4 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
